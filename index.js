f0354ae92ea28b7f7161337efca7d2857da21853
KaungThant-Zin <mrxray261@gmail.com>
2022-05-20 09:31:17 +0630

    filesave

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const program = require('commander');
 const createPassword = require('./src/createPassword');
 const chalk = require('chalk');
 const clipboardy = require('clipboardy');
+const savePassword = require('./src/savePassword');
 
 program.version("1.0.0").description("CLI password generator");
 
 program
     .option('-l, --length <number> ', 'length of password', '10')
-    .option('-s, --save ', 'save password')
+    .option('-s, --save <filename> ', 'save password')
     .option('-nn, --no-number ', 'password without number')
     .option('-ns, --no-symbol ', 'password without symbol')
     .parse();
@@ -19,7 +20,7 @@ const password = createPassword(length, number, symbol);
 clipboardy.writeSync(password)
 
 if (save) {
-    savePassword(password);
+    savePassword(password, save);
 }
 
 console.log(chalk.yellow(`password: ${password}`));
--- src/savePassword.js
+++ src/savePassword.js
@@ -1,3 +1,11 @@
-function savePassword(password) {
+const fs = require("fs");
+const path = require("path");
 
+function savePassword(password, filename = "password.txt") {
+    const filepath = path.join(__dirname, "../", filename)
+    fs.appendFile(filepath, password + "\n", 'utf-8', () => {
+        console.log(`file saved to ${filename}`)
+    })
 }
+
+module.exports = savePassword
