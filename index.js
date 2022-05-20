dccb7f831cc6a25ac97045acace1950590bdb4bb
KaungThant-Zin <mrxray261@gmail.com>
2022-05-20 08:51:18 +0630

    copy password

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const program = require('commander');
 const createPassword = require('./src/createPassword');
+const chalk = require('chalk');
+const clipboardy = require('clipboardy');
 
 program.version("1.0.0").description("CLI password generator");
 
 program
-    .option('-l, --length <number> ', 'length of password', '6')
+    .option('-l, --length <number> ', 'length of password', '10')
     .option('-s, --save ', 'save password')
     .option('-nn, --no-number ', 'password without number')
     .option('-ns, --no-symbol ', 'password without symbol')
     .parse();
 
-const { length, number, symbol } = program.opts();
+const { length, save, number, symbol } = program.opts();
 
-createPassword(length, number, symbol)
+const password = createPassword(length, number, symbol);
+
+clipboardy.writeSync(password)
+
+if (save) {
+    savePassword(password);
+}
+
+console.log(chalk.yellow(`password: ${password}`));
+console.log(chalk.greenBright("copied to clipboard"))
--- src/createPassword.js
+++ src/createPassword.js
@@ -1,6 +1,20 @@
+const alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const num = '0123456789';
+const sym = '!@#$%^&*_-+=';
 
 function createPasssword(length, number, symbol) {
-    console.log(`password created with ${length} ${number} ${symbol}`)
+    let char = alpha;
+    number && (char += num);
+    symbol && (char += sym);
+    return generateRandom(length, char);
+}
+
+function generateRandom(passlength, char) {
+    let password = "";
+    for (let i = 0; i < passlength; i++) {
+        password += char.charAt(Math.floor(Math.random() * char.length))
+    }
+    return password;
 }
 
 module.exports = createPasssword;
